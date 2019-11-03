fbc5c7ed7e4544d2ff3fb3bf1d1a8ccde436a08e
JosaRO <34759746+JosaRO@users.noreply.github.com>
2019-11-03 13:30:46 -0600

    Update login.php

--- vistas/login.php
+++ vistas/login.php
@@ -1,13 +1,11 @@
-<div class='div-centrado'>
-	<div class='form-centrado form-group' align="center">
-		<fieldset>
-			<legend align='center'>Bienvenido</legend><br>
-			<form autocomplete='off' method='POST'>
-				<input type='text' class='form-control-lg' id='user' name='user' placeholder='USUARIO'><br><br>
-				<input type='password' class='form-control-lg' id='pass' name='pass' placeholder='CONTRASEÑA'><br><br>
-				<button type='submit' class='btn btn-outline-success'>Iniciar sesión</button>
-				<button type='reset' class='btn btn-outline-secondary'>Reiniciar</button>
-			</form>
-		</fieldset>
-	</div>
+<div class='form-centrado form-group' align="center">
+	<fieldset>
+		<legend align='center'>Bienvenido</legend><br>
+		<form autocomplete='off' method='POST'>
+			<input type='text' class='form-control-lg' id='user' name='user' placeholder='USUARIO'><br><br>
+			<input type='password' class='form-control-lg' id='pass' name='pass' placeholder='CONTRASEÑA'><br><br>
+			<button type='submit' class='btn btn-outline-success'>Iniciar sesión</button>
+			<button type='reset' class='btn btn-outline-secondary'>Reiniciar</button>
+		</form>
+	</fieldset>
 </div>
