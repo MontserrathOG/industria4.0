9ee03066ca4440a89f370d38387b086bab03aa61
JosaRO <34759746+JosaRO@users.noreply.github.com>
2019-11-03 14:57:38 -0600

    Tablas para los indicadores de la maquinaria

---
+++ vistas/estadoMaquinaria.php
@@ -0,0 +1,26 @@
+<table class="table table-bordered table-hover">
+	<thead>
+		<tr class="table-warning">
+			<td align="center"><b>Maquinaria</b></td>
+			<td align="center"><b>Contador</b></td>
+			<td align="center"><b>Estado</b></td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<th scope="row">Cortadora</th>
+			<td align="center">VALOR DE BD</td>
+			<td align="center">VALOR DE BD</td>
+		</tr>
+		<tr>
+			<th scope="row">Freidora</th>
+			<td align="center">VALOR DE BD</td>
+			<td align="center">VALOR DE BD</td>
+		</tr>
+		<tr>
+			<th scope="row">Contenedor</th>
+			<td align="center">VALOR DE BD</td>
+			<td align="center">VALOR DE BD</td>
+		</tr>
+	</tbody>
+</table>
---
+++ vistas/estandarMaquinaria.php
@@ -0,0 +1,24 @@
+<table class="table table-striped table-bordered table-hover">
+	<thead>
+		<tr class="table-warning">
+			<td align="center" colspan="3"><b>Indicadores estándar</b></td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<th scope="row">Cortadora</th>
+			<td align="center">50</td>
+			<td align="center">Cambio de cuchillas</td>
+		</tr>
+		<tr>
+			<th scope="row">Freidora</th>
+			<td align="center">20</td>
+			<td align="center">Cambio de aceite</td>
+		</tr>
+		<tr>
+			<th scope="row">Contenedor</th>
+			<td align="center">20</td>
+			<td align="center">Vaciar contenedor</td>
+		</tr>
+	</tbody>
+</table>
