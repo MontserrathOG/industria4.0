5f8172340d98333ed3a0b60670273312017cd2ae
JosaRO <34759746+JosaRO@users.noreply.github.com>
2019-11-03 20:16:59 -0600

    Tabla con los datos de la producción por lotes

---
+++ vistas/estadoProduccion.php
@@ -0,0 +1,30 @@
+<table class="table table-bordered table-hover">
+	<thead>
+		<tr class="table-warning">
+			<td align="center"><b>Lote</b></td>
+			<td align="center"><b>Papas correctas</b></td>
+			<td align="center"><b>Papas quemadas</b></td>
+			<td align="center"><b>Total de latas</b></td>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<th scope="row">VALOR DE BD</th>
+			<td align="center">VALOR DE BD</td>
+			<td align="center">VALOR DE BD</td>
+			<td align="center">VALOR DE BD</td>
+		</tr>
+		<tr>
+			<th scope="row">VALOR DE BD</th>
+			<td align="center">VALOR DE BD</td>
+			<td align="center">VALOR DE BD</td>
+			<td align="center">VALOR DE BD</td>
+		</tr>
+		<tr>
+			<th scope="row">VALOR DE BD</th>
+			<td align="center">VALOR DE BD</td>
+			<td align="center">VALOR DE BD</td>
+			<td align="center">VALOR DE BD</td>
+		</tr>
+	</tbody>
+</table>
